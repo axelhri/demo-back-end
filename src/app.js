e0831be7a435588531f939f66c15acf0020b795b
axelhri <166370210+axelhri@users.noreply.github.com>
2024-12-09 18:32:01 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,15 +14,15 @@ import sendEmail from "./features/email/email.controller.js";
 connectDB();
 
 app.use(helmet());
-// app.use(
-//   rateLimit({
-//     windowMs: 15 * 60 * 1000, // 15 minutes
-//     limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-//     standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-//     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-//     // store: ... , // Redis, Memcached, etc. See below.
-//   })
-// );
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Redis, Memcached, etc. See below.
+  })
+);
 app.set("trust proxy", 1);
 app.use(mongooseSanitize());
 app.use(cors());
